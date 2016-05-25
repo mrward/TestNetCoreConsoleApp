c1b6cb3d4a03c4a7aef9aa8f06da5d380faba699
Matt Ward <ward.matt@gmail.com>
2016-05-25 22:07:43 +0100

    Add app to write out executable info for .NET Core project.

---
+++ src/NetCoreBothConsoleApp/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.IO;
+using Microsoft.DotNet.ProjectModel;
+using Microsoft.DotNet.InternalAbstractions;
+
+namespace NetCoreBothConsoleApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+                string projectJsonFileName = Path.GetFullPath("project.json");
+
+                string projectDirectory = System.IO.Path.GetDirectoryName(projectJsonFileName);
+
+                var workspace = new BuildWorkspace(ProjectReaderSettings.ReadFromEnvironment());
+                var frameworkContexts = workspace.GetProjectContextCollection(projectDirectory)
+                    .FrameworkOnlyContexts;
+
+                var rids = RuntimeEnvironmentRidExtensions.GetAllCandidateRuntimeIdentifiers();
+
+                foreach (var frameworkContext in frameworkContexts)
+                {
+                    var runtimeContext = workspace.GetRuntimeContext(frameworkContext, rids);
+
+                    OutputPaths paths = runtimeContext.GetOutputPaths("Debug");
+                    Console.WriteLine("TargetFramework: " + frameworkContext.Identity.TargetFramework);
+                    Console.WriteLine("Executable: " + paths.RuntimeFiles.Executable);
+                    Console.WriteLine();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+    }
+}
